ead3097c5ec62e955ce9a4c30b2682ba319e46c3
labstore.id@gmail.com <labstore.id@gmail.com>
2017-06-12 08:43:34 +0700

    Add existing file

--- images/large.sh
+++ images/large.sh
@@ -9,7 +9,7 @@ for FILE in `ls ./things_orig/*/*.jpg`; do
 	echo $FILE
 	FILE2=`echo $FILE|sed -e "s|things_orig|things|g"`
 	echo $FILE2
-	convert $FILE -resize 1200 $FILE2
+	convert $FILE -scale 800 $FILE2
 done
 
 #convert $1 -resize $3 -gravity center -crop 800x500+0+0 $2
